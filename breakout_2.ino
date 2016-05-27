da0a383396b65da7a1dc71d2ef113255d106d1cb
Erin Molton <erin.molton@gmail.com>
2016-05-27 18:53:43 +0100

    Remove old example code

--- breakout_2.ino
+++ breakout_2.ino
@@ -24,37 +24,6 @@ All text above, and the splash screen must be included in any redistribution
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-#if 0
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-#define LOGO16_GLCD_HEIGHT 16
-#define LOGO16_GLCD_WIDTH  16
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B00000000,
-  B00000010, B00000000,
-  B00000011, B00000000,
-  B00000011, B10000000,
-  B00000011, B11000000,
-  B00000111, B11000000,
-  B00000111, B11100000,
-  B00001111, B11100000,
-  B00011111, B11100000,
-  B00011111, B11100000,
-  B00011111, B11100000,
-  B00011111, B11100000,
-  B00001111, B11000000,
-  B00000000, B00000000,
-  B00000000, B00000000,
-  B00000000, B00000000 };
-
-//#if (SSD1306_LCDHEIGHT != 64)
-//#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-//#endif
-#endif
-
 bool bricks [6*25] = {
 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
